4886a8dbd4f292c5c169cc28a000708b747e2a56
Boris <boris.fox.solovyev@gmail.com>
2023-07-24 07:39:14 +0300

    About.py Change for my repo

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -835,7 +835,7 @@ class CommitInfo(Screen):
 
 		self.project = 0
 		self.projects = [
-			("https://api.github.com/repos/openpli/enigma2/commits" + branch, "Enigma2", API_GITHUB),
+			("https://api.github.com/repos/68foxboris/my-py3/commits" + branch, "Enigma2", API_GITHUB),
 			("https://api.github.com/repos/openpli/openpli-oe-core/commits" + branch, "Openpli Oe Core", API_GITHUB),
 			("https://api.github.com/repos/openpli/enigma2-plugins/commits" + branch_e2plugins, "Enigma2 Plugins", API_GITHUB),
 			("https://api.github.com/repos/openpli/aio-grab/commits", "Aio Grab", API_GITHUB),
