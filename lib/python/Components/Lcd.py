276d471cf9b351ac12cd04fb1c3a9262be6d0a8e
Boris <boris.fox.solovyev@gmail.com>
2023-11-01 17:32:00 +0200

    [LCD]
    * rename config.lcd.scroll_speed and config.lcd.scroll_delay settings
    * use int for config.lcd.scroll_speed and config.lcd.scroll_delay

--- lib/python/Components/Lcd.py
+++ lib/python/Components/Lcd.py
@@ -313,19 +313,13 @@ def InitLcd():
 		else:
 			config.lcd.modeminitv = ConfigNothing()
 			config.lcd.fpsminitv = ConfigNothing()
-		config.lcd.scroll_speed = ConfigSelection(choices=[
-			("500", _("Slow")),
-			("300", _("Normal")),
-			("100", _("Fast"))
-		], default="300")
-		config.lcd.scroll_delay = ConfigSelection(choices=[
-			("10000", "10 %s" % _("seconds")),
-			("20000", "20 %s" % _("seconds")),
-			("30000", "30 %s" % _("seconds")),
-			("60000", "1 %s" % _("minute")),
-			("300000", "5 %s" % _("minutes")),
-			("noscrolling", _("Off"))
-		], default="10000")
+		config.lcd.scrollSpeed = ConfigSelection(choices=[
+			(500, _("slow")),
+			(300, _("normal")),
+			(100, _("fast"))
+		], default=300)
+		delayChoices = [(x, _("%d Seconds") % x) for x in (10, 20, 30, 40, 50)] + [(x * 60, ngettext("%d Minute", "%d Minutes", x) % x) for x in (1, 2, 3, 5, 10, 15)] + [(0, _("Off"))]
+		config.lcd.scrollDelay = ConfigSelection(default=10, choices=delayChoices))
 
 		def setLCDbright(configElement):
 			ilcd.setBright(configElement.value)
@@ -746,19 +740,13 @@ def InitLcd():
 		config.lcd.fblcddisplay = ConfigNothing()
 		config.lcd.mode = ConfigNothing()
 		config.lcd.hdd = ConfigNothing()
-		config.lcd.scroll_speed = ConfigSelection(choices=[
-			("500", _("Slow")),
-			("300", _("Normal")),
-			("100", _("Fast"))
-		], default="300")
-		config.lcd.scroll_delay = ConfigSelection(choices=[
-			("10000", "10 %s" % _("seconds")),
-			("20000", "20 %s" % _("seconds")),
-			("30000", "30 %s" % _("seconds")),
-			("60000", "1 %s" % _("minute")),
-			("300000", "5 %s" % _("minutes")),
-			("noscrolling", _("Off"))
-		], default="10000")
+		config.lcd.scrollSpeed = ConfigSelection(choices=[
+			(500, _("slow")),
+			(300, _("normal")),
+			(100, _("fast"))
+		], default=300)
+		delayChoices = [(x, _("%d Seconds") % x) for x in (10, 20, 30, 40, 50)] + [(x * 60, ngettext("%d Minute", "%d Minutes", x) % x) for x in (1, 2, 3, 5, 10, 15)] + [(0, _("Off"))]
+		config.lcd.scrollDelay = ConfigSelection(default=10, choices=delayChoices)
 		config.lcd.showoutputresolution = ConfigNothing()
 		config.lcd.ledbrightness = ConfigNothing()
 		config.lcd.ledbrightness.apply = lambda: doNothing()
