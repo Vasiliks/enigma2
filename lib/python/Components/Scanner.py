564bf782c1ed3ed31ac0c20c5a8e8be4c397a7b0
Boris <boris.fox.solovyev@gmail.com>
2023-11-07 19:09:05 +0200

    Update Scanner.py

--- lib/python/Components/Scanner.py
+++ lib/python/Components/Scanner.py
@@ -1,73 +1,45 @@
+# -*- coding: utf-8 -*-
 from Plugins.Plugin import PluginDescriptor
-from Components.config import config
 from Components.PluginComponent import plugins
 
 import os
 from mimetypes import guess_type, add_type
 
-add_type("audio/dts", ".dts")
-add_type("audio/mpeg", ".mp3")
-add_type("audio/x-wav", ".wav")
-add_type("audio/x-wav", ".wave")
-add_type("audio/x-wav", ".wv")
-add_type("audio/ogg", ".oga")
-add_type("audio/ogg", ".ogg")
-add_type("audio/flac", ".flac")
-add_type("audio/mp4", ".m4a")
-add_type("audio/mpeg", ".mp2")
-add_type("audio/mpeg", ".m2a")
-add_type("audio/x-ms-wma", ".wma")
-add_type("audio/ac3", ".ac3")
-add_type("audio/x-matroska", ".mka")
-add_type("audio/x-aac", ".aac")
-add_type("audio/x-monkeys-audio", ".ape")
-add_type("audio/mp4", ".alac")
-add_type("audio/amr", ".amr")
-add_type("audio/basic", ".au")
-add_type("audio/midi", ".mid")
-add_type("video/x-dvd-iso", ".iso")
-add_type("video/x-dvd-iso", ".img")
-add_type("video/x-dvd-iso", ".nrg")
-add_type("image/jpeg", ".jpg")
-add_type("image/png", ".png")
-add_type("image/gif", ".gif")
-add_type("image/bmp", ".bmp")
-add_type("image/jpeg", ".jpeg")
-add_type("image/jpeg", ".jpe")
-add_type("image/svg+xml", ".svg")
-add_type("video/mpeg", ".mpg")
-add_type("video/dvd", ".vob")
-add_type("video/mp4", ".m4v")
-add_type("video/x-matroska", ".mkv")
-add_type("video/avi", ".avi")
-add_type("video/divx", ".divx")
-add_type("video/x-mpeg", ".dat")
-add_type("video/x-flv", ".flv")
-add_type("video/mp4", ".mp4")
-add_type("video/quicktime", ".mov")
-add_type("video/x-ms-wmv", ".wmv")
-add_type("video/x-ms-asf", ".asf")
-add_type("video/3gpp", ".3gp")
-add_type("video/3gpp2", ".3g2")
-add_type("video/mpeg", ".mpeg")
-add_type("video/mpeg", ".mpe")
-add_type("application/vnd.rn-realmedia", ".rm")
-add_type("application/vnd.rn-realmedia-vbr", ".rmvb")
-add_type("video/ogg", ".ogm")
-add_type("video/ogg", ".ogv")
-add_type("video/mp2t", ".m2ts")
-add_type("video/mts", ".mts")
-add_type("video/mp2t", ".ts")
-add_type("application/x-debian-package", ".ipk")
-add_type("application/x-dream-image", ".nfi")
-add_type("video/webm", ".webm")
-add_type("video/mpeg", ".pva")
-add_type("video/mpeg", ".wtv")
-
 
 def getType(file):
+	add_type("audio/dts", ".dts")
+	add_type("audio/x-wav", ".wave")
+	add_type("audio/x-wav", ".wv")
+	add_type("audio/mpeg", ".m2a")
+	add_type("audio/x-matroska", ".mka")
+	add_type("audio/x-aac", ".aac")
+	add_type("audio/x-monkeys-audio", ".ape")
+	add_type("audio/mp4", ".alac")
+	add_type("audio/amr", ".amr")
+	add_type("audio/midi", ".mid")
+	add_type("video/x-dvd-iso", ".iso")
+	add_type("video/x-dvd-iso", ".img")
+	add_type("video/x-dvd-iso", ".nrg")
+	add_type("video/dvd", ".vob")
+	add_type("video/avi", ".avi")
+	add_type("video/divx", ".divx")
+	add_type("video/x-mpeg", ".dat")
+	add_type("video/x-ms-asf", ".asf")
+	add_type("video/3gpp", ".3gp")
+	add_type("video/3gpp2", ".3g2")
+	add_type("application/vnd.rn-realmedia", ".rm")
+	add_type("application/vnd.rn-realmedia-vbr", ".rmvb")
+	add_type("video/ogg", ".ogm")
+	add_type("video/mp2t", ".m2ts")
+	add_type("video/mts", ".mts")
+	add_type("video/mp2t", ".ts")
+	add_type("application/x-debian-package", ".ipk")
+	add_type("application/x-dream-image", ".nfi")
+	add_type("video/mpeg", ".pva")
+	add_type("video/mpeg", ".wtv")
+
 	(type, _) = guess_type(file)
-	if not type:
+	if type is None:
 		# Detect some unknown types
 		if file[-12:].lower() == "video_ts.ifo":
 			return "video/x-dvd"
@@ -96,14 +68,14 @@ class Scanner:
 		return True
 
 	def handleFile(self, res, file):
-		if (not self.mimetypes or file.mimetype in self.mimetypes) and self.checkFile(file):
+		if (self.mimetypes is None or file.mimetype in self.mimetypes) and self.checkFile(file):
 			res.setdefault(self, []).append(file)
 
 	def __repr__(self):
 		return "<Scanner " + self.name + ">"
 
 	def open(self, list, *args, **kwargs):
-		if self.openfnc:
+		if self.openfnc is not None:
 			self.openfnc(list, *args, **kwargs)
 
 
@@ -113,37 +85,32 @@ class ScanPath:
 		self.with_subdirs = with_subdirs
 
 	def __repr__(self):
-		return self.path + str(self.with_subdirs)
+		return self.path + "(" + str(self.with_subdirs) + ")"
 
-	# we will use this in a set(), so we need to implement __hash__ and __cmp__
+	# we will use this in a set(), so we need to implement __hash__ and __eq__
 	def __hash__(self):
 		return self.path.__hash__() ^ self.with_subdirs.__hash__()
 
-	def __cmp__(self, other):
-		if self.path < other.path:
-			return -1
-		elif self.path > other.path:
-			return +1
-		else:
-			return self.with_subdirs.__cmp__(other.with_subdirs)
+	def __eq__(self, other):
+		return self.path == other.path
 
 
 class ScanFile:
-	def __init__(self, path, mimetype=None, size="", autodetect=True):
+	def __init__(self, path, mimetype=None, size=None, autodetect=True):
 		self.path = path
-		if not mimetype and autodetect:
+		if mimetype is None and autodetect:
 			self.mimetype = getType(path)
 		else:
 			self.mimetype = mimetype
 		self.size = size
 
 	def __repr__(self):
-		return self.path + str(self.mimetype)
+		return "<ScanFile " + self.path + " (" + str(self.mimetype) + ", " + str(self.size) + " MB)>"
 
 
 def execute(option):
-	print("[Scanner] execute", option)
-	if not option:
+	print("execute", option)
+	if option is None:
 		return
 
 	(_, scanner, files, session) = option
@@ -159,36 +126,49 @@ def scanDevice(mountpoint):
 			l = [l]
 		scanner += l
 
-	print("[Scanner] ", scanner)
+	print("scanner:", scanner)
 
 	res = {}
 
+	# merge all to-be-scanned paths, with priority to
+	# with_subdirs.
+
 	paths_to_scan = set()
 
-	# merge all to-be-scanned paths.
-	# first merge them all.
+	# first merge them all...
 	for s in scanner:
 		paths_to_scan.update(set(s.paths_to_scan))
+
+	# ...then remove with_subdir=False when same path exists
+	# with with_subdirs=True
+	for p in paths_to_scan.copy():
+		if p.with_subdirs == True and ScanPath(path=p.path) in paths_to_scan:
+			paths_to_scan.remove(ScanPath(path=p.path))
 
 	from Components.Harddisk import harddiskmanager
 	blockdev = mountpoint.rstrip("/").rsplit('/', 1)[-1]
 	error, blacklisted, removable, is_cdrom, partitions, medium_found = harddiskmanager.getBlockDevInfo(blockdev)
+
+	# now scan the paths
 	for p in paths_to_scan:
-		for root, dirs, files in os.walk(mountpoint):  # now scan the files paths.
+		path = os.path.join(mountpoint, p.path)
+
+		for root, dirs, files in os.walk(path):
 			for f in files:
+				path = os.path.join(root, f)
 				if (is_cdrom and f.endswith(".wav") and f.startswith("track")) or f == "cdplaylist.cdpls":
-					sfile = ScanFile(os.path.join(root, f), "audio/x-cda")
+					sfile = ScanFile(path, "audio/x-cda")
 				else:
-					sfile = ScanFile(os.path.join(root, f))
+					sfile = ScanFile(path)
 				for s in scanner:
 					s.handleFile(res, sfile)
 
-			if config.plugins.softwaremanager.scanner_with_subdirs.value:  # True choose search files in subdirs.
-				continue
-			else:
+			# if we really don't want to scan subdirs, stop here.
+			if not p.with_subdirs:
 				del dirs[:]
-		# res is a dict with scanner -> [ScanFiles]
-		return res
+
+	# res is a dict with scanner -> [ScanFiles]
+	return res
 
 
 def openList(session, files):
@@ -204,7 +184,7 @@ def openList(session, files):
 		else:
 			scanner += l
 
-	print("[Scanner] ", scanner)
+	print("scanner:", scanner)
 
 	res = {}
 
