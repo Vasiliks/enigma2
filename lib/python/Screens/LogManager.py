e83e2a8712ed4fe23e5f143dafb6444f19add20d
Boris <boris.fox.solovyev@gmail.com>
2023-11-16 18:44:36 +0200

    LogManager.py: set update list in delete all logs
    follow https://github.com/68foxboris/enigma2/commit/01b402b7c6edfeffda944b9a82fb9c1373c7240c

--- lib/python/Screens/LogManager.py
+++ lib/python/Screens/LogManager.py
@@ -284,6 +284,7 @@ class LogManager(Screen):
 			from enigma import eConsoleAppContainer
 			eConsoleAppContainer().execute("rm -f " + config.crash.debug_path.value + "*")
 			sleep(0.3)
+			self["list"].changeDir(self.defaultDir)
 			self["LogsSize"].update(config.crash.debug_path.value)
 			self["key_red"].setText("")
 			self["key_green"].setText("")
