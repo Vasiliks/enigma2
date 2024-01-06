611ca993afe559e3933bac6b87d9d44c87de0266
Boris <boris.fox.solovyev@gmail.com>
2024-01-06 09:00:25 +0200

    Small fix.

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -206,9 +206,9 @@ def countFrontpanelLEDs():
 
 
 def getRCFile(ext):
-	filename = resolveFilename(SCOPE_SKIN, pathjoin("hardware", f"{BoxInfo.getItem('rcname')}.{ext}"))
+	filename = resolveFilename(SCOPE_SKIN, pathjoin("rc_models", f"{BoxInfo.getItem('rcname')}.{ext}"))
 	if not isfile(filename):
-		filename = resolveFilename(SCOPE_SKIN, pathjoin("hardware", f"dmm1.{ext}"))
+		filename = resolveFilename(SCOPE_SKIN, pathjoin("rc_models", f"dmm1.{ext}"))
 	return filename
 
 
