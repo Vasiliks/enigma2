fd64a470152509409809bf1b9ef59167943ae573
Boris <boris.fox.solovyev@gmail.com>
2024-03-28 17:26:03 +0200

    [Added] Text border support for MultiTextContent
    [Added] Tuners string in ServiceInfoBar
    
    Follow:OpenPLi/enigma2@b1a1cf7

--- lib/python/Components/Addons/ScreenHeader.py
+++ lib/python/Components/Addons/ScreenHeader.py
@@ -8,6 +8,7 @@ from Components.MultiContent import MultiContentEntryText
 from Components.Sources.StaticText import StaticText
 
 
+
 class ScreenHeader(GUIAddon):
 	def __init__(self):
 		GUIAddon.__init__(self)
@@ -49,9 +50,9 @@ class ScreenHeader(GUIAddon):
 			if isOneItem:
 				itemHeight = self.instance.size().height()
 			if not isOneItem and idx == 0:
-				itemHeight = self.instance.size().height() * 2 // 3
+				itemHeight = self.instance.size().height()*2 // 3
 			elif idx == 1:
-				yPos = self.instance.size().height() * 2 // 3 - 3
+				yPos = self.instance.size().height()*2 // 3 - 3
 				itemHeight = self.instance.size().height() // 3
 			res.append(MultiContentEntryText(
 					pos=(0, yPos),
